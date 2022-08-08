22def01f2328ece156c185c9de70abbf91a99356
Jacob Pacheco <72306809+JacobPacheco100@users.noreply.github.com>
2022-08-07 20:03:52 -0400

    chakra ui + custom theme setup

---
+++ src/styles/theme.jsx
@@ -0,0 +1,12 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: 'red',
+      900: '#1a202c',
+    },
+  },
+})
+
+export default theme
